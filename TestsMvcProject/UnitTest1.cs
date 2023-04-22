670bcbfa06f81dba723d2bfa8ddcd2e830e3cc39
Paweł Gargula <pawel.gargula@interia.pl>
2023-04-22 14:04:11 +0200

    Add project files.

---
+++ TestsMvcProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestsMvcProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

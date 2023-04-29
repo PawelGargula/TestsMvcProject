dacad2a3242eab79c72557f5f93493ec1ef424b6
Paweł Gargula <pawel.gargula@interia.pl>
2023-04-29 11:10:52 +0200

    Add test "UploadFile"

--- TestsMvcProject/UnitTest1.cs
+++ TestsMvcProject/UnitTest1.cs
@@ -1,16 +1,38 @@
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace TestsMvcProject
 {
     public class Tests
     {
+        IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
         }
 
         [Test]
-        public void Test1()
+        public void UploadFile()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("https://localhost:7140/");
+
+            var treeBox = driver.FindElement(By.LinkText("TreeBox"));
+            treeBox.Click();
+
+            // Upload file
+            var fileUploader = driver.FindElement(By.Id("Photo"));
+            string fileName = "Typy listy.png";
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + fileName;
+            fileUploader.SendKeys(filePath);
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
         }
     }
 }

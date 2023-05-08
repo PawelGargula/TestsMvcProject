4cbbbae727c3b572aad8eee435b55a07a4835058
Paweł Gargula <pawel.gargula@interia.pl>
2023-05-08 21:46:44 +0200

    Add Scroll test

--- TestsMvcProject/UnitTest1.cs
+++ TestsMvcProject/UnitTest1.cs
@@ -1,6 +1,9 @@
 using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Reflection.Emit;
 
 namespace TestsMvcProject
 {
@@ -27,6 +30,24 @@ namespace TestsMvcProject
             string fileName = "Typy listy.png";
             string filePath = AppDomain.CurrentDomain.BaseDirectory + fileName;
             fileUploader.SendKeys(filePath);
+        }
+
+        [Test]
+        public void Scroll()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7140/");
+
+            var masterDetail = driver.FindElement(By.LinkText("MasterDetail"));
+            masterDetail.Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement ratingHeader = wait.Until(e => e.FindElement(By.CssSelector("[aria-label=\"Column Rating\"]")));
+
+            new Actions(driver)
+                .ScrollToElement(ratingHeader)
+                .Perform();
+
+            ratingHeader.Click();
         }
 
         [TearDown]
